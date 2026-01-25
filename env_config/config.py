62052bb8873190911c349801e652937676283a8e
Milosz Hoc <102030872+MiloszHocTR@users.noreply.github.com>
2026-01-25 18:18:04 +0100

    Change COOKIES_PROMPT to True

--- env_config/config.py
+++ env_config/config.py
@@ -4,4 +4,4 @@ import pathlib
 BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 PROJECT_ROOT_FOLDER = pathlib.Path(__file__).parent.parent.absolute()
 TEST_DATA_ROOT_FOLDER = f'{PROJECT_ROOT_FOLDER}/test_data'
-COOKIES_PROMPT = False
+COOKIES_PROMPT = True
